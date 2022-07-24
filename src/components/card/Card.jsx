6387547c5f2f67e07996ead68194f3f469afa51c
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-24 13:44:23 +0300

    added some styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { getContributors, getCurrentRepo } from '../actions/repos'
-import { useDispatch, useSelector } from 'react-redux'
-import { setIsFetching } from '../../reducers/reposReducer'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getContributors, getCurrentRepo } from "../actions/repos";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsFetching } from "../../reducers/reposReducer";
 
-import './card.less'
+import "./card.less";
 
-const Card = props => {
-	const dispatch = useDispatch()
-	const { username, reponame } = useParams()
-	const [repo, setRepo] = useState({ owner: {} })
-	const [contributors, setContributors] = useState([])
-	const isFetching = useSelector(state => state.repos.isFetching)
+const Card = (props) => {
+	const dispatch = useDispatch();
+	const { username, reponame } = useParams();
+	const [repo, setRepo] = useState({ owner: {} });
+	const [contributors, setContributors] = useState([]);
+	const isFetching = useSelector((state) => state.repos.isFetching);
 
 	useEffect(() => {
-		getCurrentRepo(username, reponame, setRepo)
-		getContributors(username, reponame, setContributors)
-	}, [])
+		getCurrentRepo(username, reponame, setRepo);
+		getContributors(username, reponame, setContributors);
+	}, []);
 
 	return (
 		<div>
-			<button onClick={() => props.history.goBack()} className='back-btn'>
-				BACK
+			<button onClick={() => props.history.goBack()} className="back-btn">
+				Back
 			</button>
 
 			{isFetching === false ? (
-				<div className='card'>
-					<img src={repo.owner.avatar_url} alt='' />
-					<div className='name'>{repo.name}</div>
-					<div className='stars'>{repo.stargazers_count}</div>
-					<ul>
-						{contributors.map((c, index) => (
-							<li>
-								{index + 1}. {c.login}
-							</li>
-						))}
-					</ul>
+				<div className="card">
+					<div class="img-overlay">
+						<img src={repo.owner.avatar_url} alt="" />
+					</div>
+					<div>
+						<div className="card-header">
+							<div className="name">{repo.name}</div>
+							<div className="stars">
+								⭐ <strong>{repo.stargazers_count}</strong>
+							</div>
+						</div>
+						<ul>
+							<h3>Contributors list:</h3>
+							{contributors.map((c, index) => (
+								<li>
+									{index + 1}. {c.login}
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 			) : (
-				<div className='fetching'></div>
+				<div className="fetching"></div>
 			)}
 		</div>
-	)
-}
+	);
+};
 
-export default Card
+export default Card;
--- src/components/main/repo/Repo.jsx
+++ src/components/main/repo/Repo.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import './repo.less'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./repo.less";
+import { NavLink } from "react-router-dom";
 
-const Repo = props => {
-	const repo = props.repo
+const Repo = (props) => {
+	const repo = props.repo;
 	return (
-		<div className='repo'>
-			<div className='repo-header'>
-				<div className='repo-header-name'>
-					<NavLink to={`/card/${repo.owner.login}/${repo.name}`}>
-						{repo.name}
-					</NavLink>
+		<div className="repo">
+			<div className="repo-header">
+				<div className="repo-header-name">
+					<NavLink to={`/card/${repo.owner.login}/${repo.name}`}>{repo.name}</NavLink>
 				</div>
-				<div className='repo-header-stars'>
-					Количество звезд: {repo.stargazers_count}
+				<div className="repo-header-stars">
+					⭐ <strong>{repo.stargazers_count}</strong>
 				</div>
 			</div>
-			<div className='repo-last-commit'>
-				Последний коммит: {repo.updated_at}
+			<div className="repo-last-commit">
+				Last commit at:{" "}
+				<strong>{repo.updated_at.replace(/-/g, "/").replace(/(-|T|Z)/g, " ")}</strong>
 			</div>
-			<a href={repo.html_url} class='repo-link'>
-				Ссылка на репозиторий: {repo.html_url}
+			<a href={repo.html_url} class="repo-link">
+				Link: {repo.html_url}
 			</a>
 		</div>
-	)
-}
+	);
+};
 
-export default Repo
+export default Repo;
